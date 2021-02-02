fbd21a9c48a46d7873c11e5a6741a4ecc9ab6ca5
MOHIT DILIP MAKWANA <mdmohitrc@gmail.com>
2021-02-02 19:08:31 +0530

    Update settings.py

--- DJ2/DJ2/settings.py
+++ DJ2/DJ2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd!=)*5i1zzid)x^6$)*9a*(ja%fx+*_44)p@qwuh!m2(5ya4mg'
+SECRET_KEY = 'lol'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
