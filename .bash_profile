d9514065c1c7fc44c19b0a01f556ad1c784cb9b0
mohitdmak <mdmohitrc@gmail.com>
2021-04-02 14:55:37 +0530

    db changed to mysql

---
+++ .bash_profile
@@ -0,0 +1,2 @@
+#MYSQL PATH
+export PATH=PATH:/home/mohitdmak/.local/lib/python3.8/site-packages
--- DJ2/settings.py
+++ DJ2/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os 
 from pathlib import Path
-
+from .mysqlconf import username, password
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'DJ2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogsite',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': username,
+        'PASSWORD': password,
     }
 }
 
